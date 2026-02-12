1ab8659b989f0ec2a7548b8819df33a3247a3c34
ISH k pro <princeishimwe754@gmail.com>
2026-02-12 21:21:33 +0200

    Add DashboardServlet

---
+++ src/main/java/controllers/DashboardServlet.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.getRequestDispatcher("/views/dashboard.jsp")
+                .forward(request, response);
+    }
+}
